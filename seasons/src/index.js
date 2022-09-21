e12fad69dc2c5567d1724e6dc0e6218e091cd308
Olutobilola Osunleye <losunleye@gmail.com>
2022-09-21 05:24:15 +0100

    new commit

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -12,13 +12,25 @@ import ReactDOM  from 'react-dom';
 
 // Class component
 class App extends React.Component{
+ constructor(props) {
+    super(props);
+
+    // This is the only time we do direct assignment to this.state
+    this.state = { lat:null };
+
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => {
+            //we called setState
+            this.setState({ lat: position.coords.latitude });
+             //Never do:  this.state.lat = position.coords.latitude
+        },
+        (err) => console.log(err)
+    );
+ }
+
     render() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
-        );
-        return <div>Latitude: </div>;
+        return <div>Latitude: {this.state.lat} </div>;
     }
-}
+} 
 
 ReactDOM.render(<App/>, document.querySelector('#root'))
