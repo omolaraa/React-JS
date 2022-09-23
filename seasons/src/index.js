ff329483e994cf5cef02039702ab1d9daa058487
Olutobilola Osunleye <losunleye@gmail.com>
2022-09-23 04:11:41 +0100

    Additional commit

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -16,7 +16,7 @@ class App extends React.Component{
     super(props);
 
     // This is the only time we do direct assignment to this.state
-    this.state = { lat:null };
+    this.state = { lat:null, errorMessage: '' };
 
     window.navigator.geolocation.getCurrentPosition(
         (position) => {
@@ -24,12 +24,29 @@ class App extends React.Component{
             this.setState({ lat: position.coords.latitude });
              //Never do:  this.state.lat = position.coords.latitude
         },
-        (err) => console.log(err)
+        (err) => {
+            //console.log(err)
+            this.setState({ errorMessage: err.message });
+        }
     );
  }
 
     render() {
-        return <div>Latitude: {this.state.lat} </div>;
+        // return ( 
+        // <div>
+        //     Latitude: {this.state.lat}
+        //     <br/>
+        //     Error: {this.state.errorMessage}
+        //      </div>
+        // );
+    // Conditional statement for when the positionn, error message or loading should display
+    if(this.state.errorMessage && !this.state.lat) {
+        return <div>Error: {this.state.errorMessage} </div>
+    }
+
+    if(!this.state.errorMessage && this.state.lat) {
+        return <div>Latitude: {this.state.lat} </div>
+    }
     }
 } 
 
