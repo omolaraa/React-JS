4099e714f40def6db02a85351a1b283ec5673f74
Olutobilola Osunleye <losunleye@gmail.com>
2022-10-05 05:20:25 +0100

    new

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM  from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 //Functional component
 // const App = () => {
@@ -37,8 +38,10 @@ class App extends React.Component{
     }
 
     if(!this.state.errorMessage && this.state.lat) {
-        return <div>Latitude: {this.state.lat} </div>
+        return <SeasonDisplay lat={this.state.lat} />
     }
+
+    return <div>Loading!</div>
     }
 } 
 
