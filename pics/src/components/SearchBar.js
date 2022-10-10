4bc998b323f15406c2fe653e83e72ea90347943d
Olutobilola Osunleye <losunleye@gmail.com>
2022-10-10 14:16:24 +0100

    new

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,13 +1,33 @@
 import React from "react";
 
 class SearchBar extends React.Component{
+    // Uncontrolled event handler
+    // onInputChange(event) {
+    //     console.log(event.target.value);
+    // }
+
+    state = {term: ''};
+
+    onFormSubmit(event) {
+        event.preventDefault();
+        console.log(this.state.term);
+    }
+
     render (){
         return(
             <div className="ui segment">
-                <form className="ui form">
+                <form   onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" />
+                        {/* <input type="text" onChange={this.onInputChange} /> */}
+                        {/* Alternate event handler syntex: */}
+                        {/* <input type="text" onChange={(e) => console.log(e.target.value)} /> */}
+                        {/* Controlled event handler */}
+                        <input 
+                        type="text"
+                         value={this.state.term} 
+                         onChange={e => this.setState({term: e.target.value})}
+                          />
                     </div>
                 </form>
             </div>
