8d445ff5f14f9b4c8aa7bad6de6de780dfb4989f
Olutobilola Osunleye <losunleye@gmail.com>
2022-10-12 15:05:52 +0100

    new commit

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,21 +3,25 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
  class App extends React.Component {
+    state = { images: [] };
 
-    onSearchSubmit(term){
-        axios.get('https://api.unsplash.com/search/photos', {
+    onSearchSubmit = async (term) => {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: { query: term},
             headers: {
                 Authorization:
                 'Client-ID QO3my55AOv928CIAJsKbiCosAgJgxLqx6YBizBL6HPE'
             }
         });
+
+        this.setState({ images: response.data.results });
     }
 
     render(){
     return (
         <div className="ui container" style={{marginTop : '10px'}}>
             <SearchBar submit={this.onSearchSubmit} />
+            Found: {this.state.images.length} images
         </div>
     )
     }
