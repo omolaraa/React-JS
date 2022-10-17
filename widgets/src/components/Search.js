573caa340543d9f6915fb834867ea4934855caef
Olutobilola Osunleye <losunleye@gmail.com>
2022-10-17 19:55:34 +0100

    nc

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -3,18 +3,20 @@ import axios from 'axios';
 
 const Search = () => {
     const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
 
     useEffect(() => {
         const search = async () => {
-            await axios.get('https://en.wikipedia.org/w/api.php', {
+            const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
                 params: {
                     action: 'query',
                     list: 'search',
-                    origin: '*',
+                     origin: '*',
                     format: 'json',
                     srsearch: term
-                }
+                },
             });
+            setResults(data);
         };
         search();
     },[term])
