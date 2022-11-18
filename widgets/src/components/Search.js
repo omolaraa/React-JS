20873e79e498a3d64100d53c7d59a1db83b960a3
Olutobilola Osunleye <losunleye@gmail.com>
2022-11-18 15:17:16 +0000

    New commit

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -3,8 +3,19 @@ import axios from 'axios';
 
 const Search = () => {
     const [term, setTerm] = useState('programming');
+    const [debouncedTerm, setDebouncedTerm] = useState(term);
     const [results, setResults] = useState([]);
     console.log(results);
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedTerm(term);
+        }, 1000);
+
+        return ()=>{
+            clearTimeout(timerId);
+        };
+    }, [term]);
 
     useEffect(() => {
         const search = async () => {
@@ -14,24 +25,23 @@ const Search = () => {
                     list: 'search',
                      origin: '*',
                     format: 'json',
-                    srsearch: term
+                    srsearch: debouncedTerm,
                 },
             });
             setResults(data.query.search);
         };
-
-        // Alternative to initializing term with 'programming'
-        // search() runs only when term exists:
-
-        // if(term){
-        //     search();
-        // }
         search();
-    },[term])
+    }, [debouncedTerm]);
+
 
     const renderedResults = results.map(result => {
         return(
             <div key={result.pageid} className="item">
+                <div className="right floated content">
+                    <a className="ui button" href={`https://en.wikipedia.org?curid=${result.pageid}`}> 
+                    Go
+                    </a>
+                </div>
                 <div className="content">
                     <div className="header">{result.title}</div>
                     <span dangerouslySetInnerHTML={{__html: result.snippet}}></span>
